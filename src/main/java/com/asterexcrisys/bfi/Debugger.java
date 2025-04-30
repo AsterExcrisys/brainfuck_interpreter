2286507f449ee0cd710c73b4b912db02de84fb99
user <francesconucci2005@gmail.com>
2025-04-30 12:23:04 +0200

    Laid out the structure to implement a debugger in the future

---
+++ src/main/java/com/asterexcrisys/bfi/Debugger.java
@@ -0,0 +1,8 @@
+package com.asterexcrisys.bfi;
+
+public class Debugger {
+
+    // TODO: implement debugger to allow to execute the code one step at a time
+    // TODO: add 'executeOnce' method to interface 'BlockNode'
+
+}
