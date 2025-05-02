402db53264d574d3d381e4c75fc512867d5399ea
user <francesconucci2005@gmail.com>
2025-05-02 22:26:18 +0200

    Implemented a few basic unit/integration tests

---
+++ src/test/java/com/asterexcrisys/bfi/DebuggerTest.java
@@ -0,0 +1,14 @@
+package com.asterexcrisys.bfi;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class DebuggerTest {
+
+    private Debugger debugger;
+
+    @BeforeEach
+    public void setUp() {
+        debugger = new Debugger();
+    }
+
+}
